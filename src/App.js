83091eda8b7390d0428c2fc34a72a47cce6bb834
unknown <6110110044@psu.ac.th>
2020-08-13 16:16:52 +0700

    first commit part 5 finish

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import WordCard from './WordCard';
+const word = "hello";
+function App() {
+ return (
+  <div>
+    <WordCard value="hello"/>
+  </div>
+ );
+}
+export default App;
---
+++ src/WordCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterCard from './CharacterCard.js';
+const activationHandler = c => { console.log(`${c} has been activated.`) }
+export default function WordCard(props){
+ return (
+ <div>
+    {
+        Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler}/>)
+    }
+ </div>
+ );
+}
